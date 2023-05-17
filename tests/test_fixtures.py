a8fcb6cbeecdfa5ae12d1c89d519b48f1db5f4ec
Heorhii Lazarevich <webremake@gmail.com>
2023-05-18 01:37:21 +0300

    implement tests with two different fixtures

---
+++ tests/test_fixtures.py
@@ -0,0 +1,44 @@
+"""
+Сделайте разные фикстуры для каждого теста, которые выставят размеры окна браузера
+"""
+import pytest
+from selene import browser, have
+
+
+@pytest.fixture(scope='function')
+def browser_with_window_width_1240():
+    browser.config.hold_browser_open = True
+    browser.config.browser_name = 'chrome'
+    browser.config.base_url = 'https://github.com'
+    browser.config.window_width = '1240'
+    browser.config.window_height = '768'
+    browser.config.timeout = 6.0
+
+    yield
+    browser.quit()
+
+
+@pytest.fixture(scope='function')
+def browser_with_window_width_390():
+    browser.config.hold_browser_open = True
+    browser.config.browser_name = 'chrome'
+    browser.config.base_url = 'https://github.com'
+    browser.config.window_width = '390'
+    browser.config.window_height = '600'
+    browser.config.timeout = 6.0
+
+    yield
+    browser.quit()
+
+
+def test_open_github_homepage_desktop_in(browser_with_window_width_1240):
+    browser.open('/')
+    browser.element('.HeaderMenu-link--sign-in').click()
+    assert browser.element('h1').should(have.exact_text('Sign in to GitHub'))
+
+
+def test_open_github_homepage_mobile_in(browser_with_window_width_390):
+    browser.open('/')
+    browser.element('.Button--link').click()
+    browser.element('.HeaderMenu-link--sign-in').click()
+    assert browser.element('h1').should(have.exact_text('Sign in to GitHub'))
