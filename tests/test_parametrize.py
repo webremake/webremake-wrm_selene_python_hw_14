9cfba8134ec3eaf76711eebdfc23e3ee8765388f
Heorhii Lazarevich <webremake@gmail.com>
2023-05-19 21:33:15 +0300

    implement tests with parametrized fixture

---
+++ tests/test_parametrize.py
@@ -0,0 +1,44 @@
+"""
+Переопределите параметр с помощью indirect параметризации на уровне теста
+"""
+import pytest
+from selene import browser, have
+
+
+@pytest.fixture(scope='function', params=["desktop", "mobile"])
+def browser_control(request):
+    if request.param == "desktop":
+        browser.config.hold_browser_open = True
+        browser.config.browser_name = 'chrome'
+        browser.config.base_url = 'https://github.com'
+        browser.config.window_width = '1240'
+        browser.config.window_height = '768'
+        browser.config.timeout = 6.0
+    if request.param == "mobile":
+        browser.config.hold_browser_open = True
+        browser.config.browser_name = 'chrome'
+        browser.config.base_url = 'https://github.com'
+        browser.config.window_width = '390'
+        browser.config.window_height = '600'
+        browser.config.timeout = 6.0
+
+    yield
+    browser.quit()
+
+
+@pytest.mark.parametrize("browser_control", ["desktop"], indirect=True)
+def test_open_github_homepage_desktop(browser_control):
+    browser.open('/')
+    browser.element('.HeaderMenu-link--sign-in').click()
+    assert browser.element('h1').should(have.exact_text('Sign in to GitHub'))
+
+
+browser_with_window_width_390 = pytest.mark.parametrize("browser_control", ["mobile"], indirect=True)
+
+
+@browser_with_window_width_390
+def test_open_github_homepage_mobile(browser_control):
+    browser.open('/')
+    browser.element('.Button--link').click()
+    browser.element('.HeaderMenu-link--sign-in').click()
+    assert browser.element('h1').should(have.exact_text('Sign in to GitHub'))
