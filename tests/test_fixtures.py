44db8bdef08ad8c8d66e44cdfbe3a6972c52f7ff
Heorhii Lazarevich <webremake@gmail.com>
2023-05-27 20:35:31 +0300

    refactor fixtures code in test_fixtures.py

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -5,39 +5,37 @@ import pytest
 from selene import browser, have
 
 
-@pytest.fixture(scope='function')
-def browser_with_window_width_1240():
+@pytest.fixture(scope='function',  autouse=True)
+def browser_control():
     browser.config.hold_browser_open = True
     browser.config.browser_name = 'chrome'
     browser.config.base_url = 'https://github.com'
+    browser.config.timeout = 6.0
+
+
+@pytest.fixture(scope='function')
+def browser_window_width_1240():
     browser.config.window_width = '1240'
     browser.config.window_height = '768'
-    browser.config.timeout = 6.0
-
     yield
     browser.quit()
 
 
 @pytest.fixture(scope='function')
-def browser_with_window_width_390():
-    browser.config.hold_browser_open = True
-    browser.config.browser_name = 'chrome'
-    browser.config.base_url = 'https://github.com'
+def browser_window_width_390():
     browser.config.window_width = '390'
     browser.config.window_height = '600'
-    browser.config.timeout = 6.0
-
     yield
     browser.quit()
 
 
-def test_open_github_homepage_desktop_in(browser_with_window_width_1240):
+def test_open_github_homepage_desktop_in(browser_window_width_1240):
     browser.open('/')
     browser.element('.HeaderMenu-link--sign-in').click()
     assert browser.element('h1').should(have.exact_text('Sign in to GitHub'))
 
 
-def test_open_github_homepage_mobile_in(browser_with_window_width_390):
+def test_open_github_homepage_mobile_in(browser_window_width_390):
     browser.open('/')
     browser.element('.Button--link').click()
     browser.element('.HeaderMenu-link--sign-in').click()
