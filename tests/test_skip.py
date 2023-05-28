752c64689dead4bfc58cfc0dc6539969446d3eb3
Heorhii Lazarevich <webremake@gmail.com>
2023-05-28 15:15:42 +0300

    refactor test functions code in test_skip.py

--- tests/test_skip.py
+++ tests/test_skip.py
@@ -1,51 +1,38 @@
-"""
-Параметризуйте фикстуру несколькими вариантами размеров окна
-Пропустите мобильный тест, если соотношение сторон десктопное (и наоборот)
-"""
 import pytest
 from selene import browser, have
 
 
-@pytest.fixture(scope='function', params=["desktop", "mobile"])
-def browser_control(request):
+@pytest.fixture(scope='function', autouse=True)
+def browser_setup():
     browser.config.hold_browser_open = True
     browser.config.browser_name = 'chrome'
     browser.config.base_url = 'https://github.com'
     browser.config.timeout = 6.0
-    if request.param == "desktop":
-        browser.config.window_width = '1240'
-        browser.config.window_height = '768'
-
-    if request.param == "mobile":
-        browser.config.window_width = '390'
-        browser.config.window_height = '600'
     yield
     browser.quit()
 
 
-@pytest.mark.parametrize(
-    "browser_control",
-    [
-        "desktop",
-        pytest.param("mobile", marks=pytest.mark.skip(reason="Пропускаем тест для ширины экрана mobile"))
-    ],
-    indirect=True
-)
-def test_open_github_homepage_desktop(browser_control):
+@pytest.fixture(scope='function', params=["desktop", "mobile"])
+def window_size(request):
+    if request.param == "desktop":
+        browser.config.window_width = '1240'
+        browser.config.window_height = '768'
+    elif request.param == "mobile":
+        browser.config.window_width = '390'
+        browser.config.window_height = '600'
+
+
+def test_open_github_homepage_desktop(window_size):
+    if browser.config.window_width == '390':
+        pytest.skip("Пропускаем тест для ширины экрана mobile")
     browser.open('/')
     browser.element('.HeaderMenu-link--sign-in').click()
     assert browser.element('h1').should(have.exact_text('Sign in to GitHub'))
 
 
-@pytest.mark.parametrize(
-    "browser_control",
-    [
-        "mobile",
-        pytest.param("desktop", marks=pytest.mark.skip(reason="Пропускаем тест для ширины экрана desktop"))
-    ],
-    indirect=True
-)
-def test_open_github_homepage_mobile(browser_control):
+def test_open_github_homepage_mobile(window_size):
+    if browser.config.window_width == '1240':
+        pytest.skip("Пропускаем тест для ширины экрана desktop")
     browser.open('/')
     browser.element('.Button--link').click()
     browser.element('.HeaderMenu-link--sign-in').click()
